1b44470045aa6e36d884166b210b75a810c67a5c
Benjamin Milde <benni@kobrakai.de>
2019-03-03 15:46:09 +0100

    Initial Commit

---
+++ lib/brick.ex
@@ -0,0 +1,18 @@
+defmodule Brick do
+  @moduledoc """
+  Documentation for Brick.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Brick.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
