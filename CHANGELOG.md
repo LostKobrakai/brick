bb7f9b7f8e0b0120b858a2958b8959be3b6e8103
Benjamin Milde <benni@kobrakai.de>
2019-03-03 16:27:56 +0100

    Make it ready for hex.pm

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.1.0] - 2019-03-03
+### Added
+- Basic functionality for defining and rendering components
--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Brick.MixProject do
   use Mix.Project
 
+  @github "https://github.com/LostKobrakai/brick"
+
   def project do
     [
+      name: "Brick",
+      source_url: @github,
       app: :brick,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -25,6 +31,20 @@ defmodule Brick.MixProject do
       {:phoenix_html, "~> 2.11", optional: true},
       {:jason, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Brick is a component library bases on Phoenix.View and Phoenix.Template."
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github
+      }
     ]
   end
 end
